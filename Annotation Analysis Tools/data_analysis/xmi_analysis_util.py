4ccc3c1b17b9b7c4a13d2b21194367fac28a19ef
Bruno Brocai <ra236@stud.uni-heidelberg.de>
2023-08-11 15:16:08 +0200

    Restructured analysis files and added corpus classes. Additional analysis functions.

--- Annotation Analysis Tools/data_analysis/xmi_analysis_util.py
+++ Annotation Analysis Tools/data_analysis/xmi_analysis_util.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def inside_of(coord_list, coord_tuple):
     """
     Checks whether the specific coordinates
@@ -34,6 +37,31 @@ def label_associations(moral_spans_list, label_spans_list):
     return dictionary
 
 
+def label_associations_category(moral_spans_list, label_spans_list):
+    """
+    Some moralizing spans have no instance of a specific phenomenon
+    (such as protagonists), others have many. It can be useful (when looking
+    for specific examples) to have a way to find them. This function should
+    make it easy to find all instances of a phenomenon.
+    """
+
+    dictionary = {}
+
+    for moralization in moral_spans_list:
+        dictionary[moralization] = []
+    for label in label_spans_list:
+        moral_span = inside_of(
+            moral_spans_list, label["Coordinates"])
+        try:
+            dictionary[moral_span].append(label)
+        except KeyError:
+            print("".join(("KeyError: ",
+                           str(label),
+                           " -- Is the label inside a moralization?")))
+
+    return dictionary
+
+
 def get_span(text, coordinates):
     try:
         span = text[coordinates[0]:(coordinates[1])]
@@ -53,3 +81,128 @@ def special_upper(string):
             newstring = ''.join((newstring, string[i]))
 
     return newstring
+
+
+def label_in_list(list, category):
+    for element in list:
+        if category in element.values():
+            return True
+    return False
+
+
+def possible_labels(category):
+    cat_possibilities = ['obj_morals', 'subj_morals', 'all_morals',
+                         'prot_roles', 'prot_groups', 'prot_ownother',
+                         'com_functions',
+                         'demands']
+    if category in cat_possibilities[:3]:
+        return [
+            'Care',
+            'Harm',
+            'Fairness',
+            'Cheating',
+            'Loyalty',
+            'Betrayal',
+            'Authority',
+            'Subversion',
+            'Sanctity',
+            'Degradation',
+            'Liberty',
+            'Oppression',
+            'OTHER'
+        ]
+    if category == cat_possibilities[3]:
+        return [
+            'Adresassat:in',
+            'Benefizient:in',
+            'Forderer:in',
+            'Malefizient:in',
+            'Kein Bezug',
+            'Bezug unklar'
+        ]
+    if category == cat_possibilities[4]:
+        return [
+                "Individuum",
+                "Institution",
+                "Menschen",
+                "soziale Gruppe",
+                "Sonstige"
+            ]
+    if category == cat_possibilities[5]:
+        return [
+            'Own Group',
+            'Other Group',
+            'Neutral',
+        ]
+    if category == cat_possibilities[6]:
+        return [
+            'Appell',
+            'Beziehung',
+            'Darstellung',
+            'Expression',
+            'Appell+Beziehung',
+            'Appell+Darstellung',
+            'Appell+Expression'
+        ]
+    if category == cat_possibilities[7]:
+        return [
+            'implizit',
+            'explizit'
+        ]
+    return None
+
+
+def calculate_normalized_pmi(contingency_table):
+    """
+    Calculate PMI (Pointwise Mutual Information)
+    from a given contingency table.
+
+    Parameters:
+        - contingency_table: A 2x2 contingency table with counts.
+
+    Returns:
+        - PMI value.
+    """
+    counter_12, counter_1 = contingency_table[0]
+    counter_2, counter_none = contingency_table[1]
+
+    total_segments = counter_12 + counter_1 + counter_2 + counter_none
+
+    p_x_and_y = counter_12
+    p_x = (counter_12 + counter_1)
+    p_y = (counter_12 + counter_2)
+
+    if p_x_and_y == 0.0 or p_x == 0.0 or p_y == 0.0:
+        return float('-inf')  # Avoid log(0)
+
+    pmi = np.log2(p_x_and_y / ((p_x * p_y) / total_segments))
+    jsi = np.log2(p_x_and_y / total_segments)
+    return (pmi / (-1 * jsi))
+
+
+def freq_table(corpus, associations1, associations2, label1, label2):
+    counter_1 = 0
+    counter_2 = 0
+    counter_12 = 0
+    counter_none = 0
+
+    for moralization in corpus.moralizations:
+        if label_in_list(associations1[moralization], label1):
+            if label_in_list(associations2[moralization], label2):
+                counter_12 += 1
+            else:
+                counter_1 += 1
+        elif label_in_list(associations2[moralization], label2):
+            counter_2 += 1
+        else:
+            counter_none += 1
+
+    try:
+        contingency_table = [
+            [counter_12, counter_1],
+            [counter_2, counter_none]
+        ]
+    except ZeroDivisionError:
+        print("Error: Division by zero.")
+
+    return contingency_table
